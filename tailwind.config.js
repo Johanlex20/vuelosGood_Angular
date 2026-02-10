443da36ed4ecc17a427e30051f54e26fbeb686c0
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2026-02-09 20:13:34 -0500

    DashBoar Listado en creacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['"Google Sans Flex"', 'sans-serif'],
+        google: ['"Google Sans Flex"', 'sans-serif'],
+      },
+      colors: {
+        'blue-dark': '#001a4a',
+        'blue-dark-medium': '#034579',
+        'blue-main': '#008abb',
+        'blue-light': '#95cadc',
+        'blue-light-very': '#d4e7ed'
+    },
+    borderRadius:{
+      'dashboard':'2rem', //  32px
+    }
   },
   plugins: [],
+  }
 }
-
